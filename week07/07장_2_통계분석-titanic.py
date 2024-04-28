80dedc2232215a048474afdb255453ee9a3cc89f
jangjieun0675 <jangji0513@naver.com>
2024-04-28 16:57:37 +0900

    titanic.py 설명 추가

--- week07/07장_2_통계분석-titanic.py
+++ week07/07장_2_통계분석-titanic.py
@@ -6,22 +6,26 @@ Created on Thu Mar 18 22:32:51 2021
 """
 import seaborn as sns
 import pandas as pd
-titanic = sns.load_dataset("titanic")
+titanic = sns.load_dataset("titanic") #타이타닉 데이터프레임
 titanic.to_csv('./DATA/titanic.csv', index = False)
 
-titanic.isnull().sum()
+titanic.isnull().sum()# null의 개수
+#age의 중간값을 null에 채움
 titanic['age'] = titanic['age'].fillna(titanic['age'].median())
 titanic['embarked'].value_counts()
+#Southampton으로 embark_town의 null값을 채움
 titanic['embark_town'] = titanic['embark_town'].fillna('Southampton')
 titanic['deck'].value_counts()
+#C로 deck의 null값을 채움
 titanic['deck'] = titanic['deck'].fillna('C')
 titanic.isnull().sum()
 
 titanic.info()
-titanic.survived.value_counts()
+titanic.survived.value_counts()#사망자 : 549명, 생존자 : 342명, 총 891명
 
 import matplotlib.pyplot as plt
 f,ax = plt.subplots(1, 2, figsize = (10, 5))
+#남성 생존자와 여성 생존자를 나누어 그래프 출력
 titanic['survived'][titanic['sex'] == 'male'].value_counts().plot. \
     pie(explode = [0,0.1], autopct = '%1.1f%%', ax = ax[0], shadow = True)
 titanic['survived'][titanic['sex'] == 'female'].value_counts().plot. \
@@ -36,6 +40,8 @@ plt.show()
 
 titanic_corr = titanic.corr(method = 'pearson', numeric_only=1)
 titanic_corr
+#survived에 제일 영향을 미치는 것은 fare이다.
+#pclass가 높을수록 생존률이 낮아진다
 titanic_corr.to_csv('DATA/titanic_corr.csv', index = False)
 
 titanic['survived'].corr(titanic['adult_male'])
