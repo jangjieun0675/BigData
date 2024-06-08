7b9195e58e724025ee16586da452f06419458488
jangjieun0675 <jangji0513@naver.com>
2024-06-09 02:58:42 +0900

    데이터 탐색 정리 수정

--- week13/데이터탐색_final 정리.py
+++ week13/데이터탐색_final 정리.py
@@ -78,6 +78,153 @@ data2=data1.copy()
 data2['sales_new'] = np.where(pd.notnull(data2['sales']) == True, data2['sales'], data2['salary'])
 
 
+# In[5-1]: SQL 질의문으로 나타내기
+# csv파일 (data.csv) 생성하기
+import pandas as pd
+import numpy as np
+data = {
+    "메뉴":['아메리카노','카페라떼','카페모카', '바닐라라떼', '녹차', '초코라떼', '바닐라콜드브루'],
+    "가격":[4100, 4600, 4600, 5100, 4100, 5000, 5100],
+    "할인율":[0.5, 0.1, 0.2, 0.3, 0, 0, 0],
+    "칼로리":[10, 180, 420, 320, 20, 500, 400],
+    "원두":['콜롬비아', np.NaN, '과테말라', np.NaN, '한국', '콜롬비아', np.NaN],
+    "이벤트가":[1900, 2300, np.NaN, 2600, np.NaN, 3000, 3200],
+}
+df = pd.DataFrame(data)
+
+# 데이터 불러오기
+df
+
+# 문제 [1] 1개 조건, 할인율 > 0.2인 메뉴에 대한 정보를 보기
+cond=df.할인율 > 0.2
+df[cond]
+
+# 문제 [2] 2개 조건, 할인율 > 0.2 AND 칼로리 < 200인 메뉴에 대한 정보를 보기
+cond1=df.할인율 >= 0.2; cond2=df.칼로리 < 200;
+df[cond1 & cond2]
+
+# 문제 [3] 2개 조건, 할인율 > 0.2 OR 칼로리 < 200인 메뉴에 대한 정보를 보기
+cond1=df.할인율 >= 0.2; cond2=df.칼로리 < 200;
+df[cond1 | cond2]
+
+# 문제 [4] 문자열 1개 조건, 원두 이름이 "콜롬비아"인 메뉴에 대한 정보를 보기
+cond1=df.원두=='콜롬비아'
+df[cond1]
+
+# 문제 [5] 문자열 조건, 원두 이름이 "콜롬비아" AND 가격 < 4500인 메뉴에 대한 정보를 보기
+cond1=df.원두=='콜롬비아'; cond2=df.가격 < 4500;
+df[cond1 & cond2]
+
+# 문제 [6] 컬럼별 결측치 갯수 확인
+df.isnull().sum()
+
+# 문제 [7] 결측값 채우기, 원두 결측치를 "코스타리카"로 채우기
+df['원두']=df['원두'].fillna('코스타리카')
+
+# 문제 [8] 결측값 채우기, '이벤트가'컬럼 결측치는 1900으로 결측치 채움
+df['이벤트가']=df['이벤트가'].fillna(1900)
+
+# 문제 [9] 데이터 값 변경하기 : 아메리카노 -> 룽고, 녹차 -> 그린티, replace 문 사용해보기
+df.메뉴.replace('아메리카노', '룽고', inplace=True)
+df.메뉴.replace('녹차', '그린티', inplace=True)
+
+# 문제 [10] 이벤트가격 1900 -> 1500 변경하기, replace 사용
+df.이벤트가.replace(1900, 1500, inplace=True)
+
+# 문제 [11] 메뉴 "바닐라라떼"의 원두를 "과테말라"로 변경, df.loc 사용
+df.loc[3, "원두"]="과테말라"
+
+# 문제 [12] 이벤트가 전체를 1000으로 변경
+df['이벤트가']=1000
+
+# 문제 [13] 컬럼별 데이터 갯수 카운트, NULL 값은 제외됨, count() 사용, 기본값 axis=0
+df.count()
+
+# 행별 데이터 갯수 카운트, NULL 값은 제외됨, count() 사용, axis=1
+df.count(axis=1)
+
+# 문제 [14] 데이터 갯수 len() 함수 사용, shape 사용
+len(df)
+df.shape
+df.shape[0]
+
+# 가격 컬럼의 최대값 구하기
+df.가격.max()
+
+# 문제 [15] 가격 컬럼의 최소값 구하기
+df.가격.min()
+
+# 가격 컬럼의 평균값 구하기
+df.가격.mean()
+
+# 문제 [16] 가격 컬럼의 중앙값 구하기
+df.가격.median()
+
+# 합계
+df.가격.sum()
+
+# 가격 컬럼의 표준편차 구하기
+df.가격.std()
+
+# 분산
+df.가격.var()
+
+# 백분위수 알아보기
+df['가격'].describe()
+
+# 가격 컬럼의 하위 25% 값 구하기, quantile() 함수 사용
+df.가격.quantile(.25)
+
+# 가격 컬럼의 하위 25% 데이터 모두 보기
+cond=(df.가격 <= df.가격.quantile(.25))
+df[cond]
+
+# 최빈값 구하기, mode(), 주의 : 결과는 Series이다.
+df.원두.mode()[0]
+
+# apply 함수 예시
+def cal(x):
+  if x>=300:
+    return 'No'
+  else:
+    return 'Yes'
+df['칼로리'].apply(cal)
+
+# apply 적용해서 새로운 컬럼 생성 (칼로리 컬럼 활용)
+df['먹어도될까요']=df['칼로리'].apply(cal)
+
+#그룹(groupby)
+# 원두별 가격 평균 구하기
+df.groupby('원두').mean()
+
+# (원두와 할인율)별 평균값 구하기
+df.groupby(['원두', '할인율']).mean()
+
+# (원두와 할인율)별 가격 평균값 구하기
+group1= df.groupby(['원두', '할인율'])['가격'].mean()
+
+# 1개 인덱스 형태로 리셋
+group2=df.groupby(['원두', '할인율'])['가격'].mean().reset_index()
+
+# group2에서 원두='콜롬비아' AND 할인율=0.0 의 가격
+print(group2.index)
+cond1= (group2.원두=='콜롬비아')
+cond2= (group2.할인율==0.0)
+group2[cond1 & cond2]['가격']
+
+# 결측치 채우기 : '이벤트가' 컬럼 결측치는 이벤트가격 데이터 중 최소값으로 결측치 채움
+m=df.이벤트가.min()
+df.이벤트가.fillna(m, inplace=True)
+
+# 결측치 채우기 :'원두' 컬럼 결측치는 원두 데이터 중 최빈값으로 결측치 채움
+m=df.원두.mode()[0]
+df.원두.fillna(m, inplace=True)
+
+# 가격이 5000 이상인 데이터의 개수를 구하시오
+cond=df.가격 >= 5000
+len(df[cond])
+
+
 # In[5]: SQL 질의문으로 나타내기
 
 #[질의 3-1] emp.csv를 읽어서 DataFrame emp 만들기
@@ -184,7 +331,6 @@ df2['NEWSAL'] = df2['OLDSAL']
 #[질의 5-3] emp의 oldsal 컬럼을 삭제하여라
 #(ALTER TABLE emp DROP COLUMN oldsal;
 df2 = df2.drop('OLDSAL', axis=1)
-
 
 
 
